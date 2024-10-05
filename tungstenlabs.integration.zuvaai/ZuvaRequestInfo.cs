6172e29e61916386cd0af2ce28ae4ff084812580
RafaelCastroAtTungstenAutomation <rafael.castro@tungstenautomation.com>
2024-10-05 13:40:17 -0400

    Initial commit

---
+++ tungstenlabs.integration.zuvaai/ZuvaRequestInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tungstenlabs.integration.zuvaai
+{
+    public class ZuvaRequestInfo
+    {
+        public string ZuvaFileID { get; set; }
+        public string ZuvaToken { get; set; }
+        public List<ZuvaFieldConfig> ZuvaFieldConfigCollection { get; set; }
+    }
+
+    public class ZuvaFieldConfig
+    {
+        public string ZuvaFieldIDs { get; set; }
+        public short TransformationType { get; set; } //0-None, 1-First, 2-Concat all
+        public short NormalizationType { get; set; } //1-Date, 2-Currency, 3-Durations
+    }
+}
